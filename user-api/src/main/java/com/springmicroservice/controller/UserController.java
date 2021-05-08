a4357f7d87efbc02c72686feb9b621a07deea721
dhanamjaya123 <dhanu8121@gmail.com>
2021-05-08 21:53:49 +0530

    update the code

--- user-api/src/main/java/com/springmicroservice/controller/UserController.java
+++ user-api/src/main/java/com/springmicroservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.springmicroservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +11,13 @@ import com.springmicroservice.model.User;
 
 @RestController
 public class UserController {
+	
+	@Autowired
+	private Environment env;
+	
 	@PostMapping("/user")
 	public ResponseEntity<?> createUser(@RequestBody User user) {
-		return ResponseEntity.ok().body("Successfully saved :" + user.getUsername());
+		return ResponseEntity.ok().body("Successfully saved :" + env.getProperty("token.name"));
 	}
 
 }
